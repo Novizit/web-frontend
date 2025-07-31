65c51663a979cee2706962cd7d815b86ad37e47e
Your Name <venkatkiran343@gmail.com>
2025-07-31 12:46:57 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Enable standalone output for Docker
-  output: 'standalone',
-  
   // Optimize images
   images: {
     domains: ['novizitstorage.blob.core.windows.net'],
     formats: ['image/webp', 'image/avif'],
-  },
-  
-  // Enable experimental features
-  experimental: {
-    optimizePackageImports: ['swiper'],
   },
   
   // Security headers
@@ -41,25 +33,6 @@ const nextConfig: NextConfig = {
   // Environment variables
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
-  
-  // Webpack configuration
-  webpack: (config, { dev, isServer }) => {
-    // Optimize bundle size
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
-        },
-      };
-    }
-    
-    return config;
   },
 };
 
